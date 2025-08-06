b0e23dc7d62ad317ba67bb02d216ee41b9ae62d1
Cao Trung Hiếu <trunghieu.personal@gmail.com>
2025-08-06 14:55:23 +0000

    test: Add dump html, css and login page

--- tahp/hooks.py
+++ tahp/hooks.py
@@ -32,7 +32,7 @@ fixtures = [
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/tahp/css/tahp.css"
+app_include_css = "/assets/tahp/css/tahp.css"
 # app_include_js = "/assets/tahp/js/tahp.js"
 
 # include js, css files in header of web template
