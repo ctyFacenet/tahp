e43ba1d99356432b7ddb776add5765bfb6282dc5
Cao Trung Hiếu <trunghieu.personal@gmail.com>
2025-08-08 17:16:39 +0000

    fix: Change error during running setup.py

--- tahp/setup.py
+++ tahp/setup.py
@@ -10,15 +10,13 @@ def setup_website():
 
     # Đây là URL công khai sau khi build
     logo_url = "/assets/tahp/images/tahp.svg"
-    app_name = "Trường An Hải Phòng"
-    language = "vi"
 
     frappe.db.set_value("Website Settings", "Website Settings", "brand_html", f"<img src='{logo_url}' height='30px'>")
     frappe.db.set_value("Website Settings", "Website Settings", "favicon", logo_url)
     frappe.db.set_value("Website Settings", "Website Settings", "app_logo", logo_url)
     frappe.db.set_value("Website Settings", "Website Settings", "splash_image", logo_url)
-    frappe.db.set_value("Website Settings", "Website Settings", "app_name", app_name)
-    frappe.db.set_value("System Settings", "System Settings", "language", language)
+    frappe.db.set_value("Website Settings", "Website Settings", "app_name", "Trường An Hải Phòng")
+    frappe.db.set_value("System Settings", "System Settings", "language", "vi")
     frappe.db.commit()
 
     print("✅ Cài đặt logo & tên app và ngôn ngữ thành công.")
