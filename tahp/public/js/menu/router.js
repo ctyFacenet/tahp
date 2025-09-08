151e2c94ee5243214beb897985b7c19539ab12e8
Son Minh <kienthanh2seri@gmail.com>
2025-09-08 10:00:54 +0000

    App menu redirect , reload page when modern menu screen

--- tahp/public/js/menu/router.js
+++ tahp/public/js/menu/router.js
@@ -1,8 +1,19 @@
 frappe.router.render = function () {
   if (this.current_route[0]) {
+    console.log("Rendering route:", this.current_route[0]);
+
+    if (this.current_route[0] === "modern-menu") {
+      if (!sessionStorage.getItem("modern_menu_reloaded")) {
+        sessionStorage.setItem("modern_menu_reloaded", "1");
+        location.reload();
+        return;
+      }
+    } else {
+      sessionStorage.removeItem("modern_menu_reloaded");
+    }
+
     this.render_page();
   } else {
-    // Show our menu page
     frappe.set_route(['app', 'modern-menu']);
   }
-}
+};
