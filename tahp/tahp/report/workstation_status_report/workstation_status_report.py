54cab521a89e3e89927073ab401203fc0e9723a8
Cao Trung Hiếu <trunghieu.personal@gmail.com>
2025-09-29 14:27:16 +0000

    feat(report): Add summary report of produced items

--- tahp/tahp/report/workstation_status_report/workstation_status_report.py
+++ tahp/tahp/report/workstation_status_report/workstation_status_report.py
@@ -1,11 +1,9 @@
 # Copyright (c) 2025, FaceNet and contributors
 # For license information, please see license.txt
 
-# import frappe
-
+import frappe
 
 def execute(filters=None):
-	columns, data = [], []
 	columns = [
 		{
 			"fieldname": "workstation",
@@ -43,5 +41,41 @@ def execute(filters=None):
 			"fieldtype": "Time",
 			"width": 100
 		},
+		{
+			"fieldname": "productivity",
+			"label": "Hiệu suất sử dụng",
+			"fieldtype": "Data",
+			"width": 100
+		},
+		{
+			"fieldname": "group_name",
+			"label": "Phân loại lý do dừng",
+			"fieldtype": "Data",
+			"width": 100
+		},
+		{
+			"fieldname": "reason",
+			"label": "Lý do dừng chi tiết",
+			"fieldtype": "Data",
+			"width": 150
+		},
+		{
+			"fieldname": "employee",
+			"label": "Người giám sát",
+			"fieldtype": "Link",
+			"options": "Employee",
+			"width": 100
+		},
 	]
+
+	data = None
+	print('hi')
+	workstations = frappe.db.get_all("Workstation", fields=["status","name","custom_parent","custom_is_parent"])
+	groups = dict()
+	for workstation in workstations:
+		if workstation.custom_is_parent:
+			if workstation.name not in groups:
+				groups[workstation.name] = []
+			
+
 	return columns, data
