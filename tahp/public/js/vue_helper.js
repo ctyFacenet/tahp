37729ff29d5b88564f3627f67a2f8791047c254f
Son Minh <kienthanh2seri@gmail.com>
2025-10-14 07:10:20 +0000

    fix: Mount 2 app cùng 1 el

--- tahp/public/js/vue_helper.js
+++ tahp/public/js/vue_helper.js
@@ -4,12 +4,21 @@ import "ant-design-vue/dist/reset.css";
 
 export function mountVue(Component, props, wrapper) {
   const el = wrapper.get ? wrapper.get(0) : wrapper;
+
+  if (el.__vue_app__) {
+    el.__vue_app__.app.unmount();
+    el.innerHTML = "";
+  }
+
   const app = createApp(Component, props);
   app.use(Antd);
   SetVueGlobals(app);
   const vm = app.mount(el);
-  return { app, vm, el };
+
+  el.__vue_app__ = { app, vm, el };
+  return el.__vue_app__;
 }
+
 
 export function unmountVue(mountedApp) {
   if (mountedApp?.app) {
