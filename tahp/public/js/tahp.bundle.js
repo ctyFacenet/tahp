c5d5636d6be57be68f1a72c2796ce8e7e6233e72
Son Minh <kienthanh2seri@gmail.com>
2025-10-29 08:27:35 +0000

    remove custom form

--- tahp/public/js/tahp.bundle.js
+++ tahp/public/js/tahp.bundle.js
@@ -1,7 +1,6 @@
 import { mountVue, unmountVue } from "./vue_helper";
 import "./components/custom_field.bundle.js";
 import "./components/dialog.bundle.js";
-import "./components/form_view.bundle.js";
 import "./components/grid_view.bundle.js";
 import "./components/hello_world.bundle.js";
 import "./components/list_view.bundle.js";
