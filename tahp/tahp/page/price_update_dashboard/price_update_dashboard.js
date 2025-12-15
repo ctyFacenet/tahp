3d3a36d4f62cda6084e1efe57d14b1eae2befebe
Cao Trung Hiếu <trunghieu.personal@gmail.com>
2025-12-15 04:04:47 +0000

    chore(pms): remose unused page for price update

--- tahp/tahp/page/price_update_dashboard/price_update_dashboard.js
+++
@@ -1,7 +0,0 @@
-frappe.pages['price-update-dashboa'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Price Update Dashboard',
-		single_column: true
-	});
-}
