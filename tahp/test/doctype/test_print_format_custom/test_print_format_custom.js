da8690b041b8342562bd27e80a2cdaac1522530e
The Citizen <kienthanh2seri@gmail.com>
2025-12-16 08:41:00 +0000

    remove(file): test print format custom

--- tahp/test/doctype/test_print_format_custom/test_print_format_custom.js
+++ tahp/test/doctype/test_print_format_custom/test_print_format_custom.js
@@ -1,16 +1,7 @@
 // Copyright (c) 2025, FaceNet
 // License: See license.txt
 
-frappe.ui.form.on("Test print format custom", {
-  refresh(frm) {
-    const meta = frappe.get_meta(frm.doc.doctype);
-    let label_map = {};
-    meta.fields.forEach(f => {
-      if (f.fieldname && f.label) {
-        label_map[f.fieldname] = f.label;
-      }
-    });
-    frm.doc._labels = label_map;
-
-  }
-});
+// frappe.ui.form.on("Test print format custom", {
+//   refresh(frm) {
+//   }
+// });
