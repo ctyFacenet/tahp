cddc4fb6d9bd5ce4ae0f6d9c52429a409cc135cf
phucfix <186391026+phucfix@users.noreply.github.com>
2025-12-27 09:04:54 +0000

    feat(pms): add purchase approval button in material request

--- tahp/pms/doc_events/material_request/material_request_button.py
+++ tahp/pms/doc_events/material_request/material_request_button.py
@@ -1,31 +1,43 @@
 import frappe
-from frappe.utils import now, now_datetime, today, getdate
+from frappe.model.mapper import get_mapped_doc
+from frappe.utils import now_datetime
 import json
 
 @frappe.whitelist()
-def create_purchase_approval(material_request, supplier, items, payment_method, payment_note, delivery_address, delivery_note, delivery_amount_note):
-    doc = frappe.new_doc("Purchase Approval")
-    doc.supplier = supplier
-    doc.posting_date = now()
-
-    if isinstance(items, str):
-        items = json.loads(items)
+def create_purchase_approval(material_request, supplier, selected_items, payment_method, payment_note, delivery_address, delivery_note, delivery_amount_note):
+    if isinstance(selected_items, str):
+        selected_items = json.loads(selected_items)
     
-    # frappe.session.user
-    for item in items:
-        doc.append("items", {
-            "item_code": item["item_code"],
-            "item_name": item["item_name"],
-            "qty": item["qty"],
-            "stock_uom": item["uom"]
-        })
-
-    # Mandatory field
+    qty_map = {item["item_code"]: item["qty"] for item in selected_items}
+    selected_item_codes = set(qty_map.keys())
+    
+    # Material Request Item -> Purchase Approval Item
+    doc = get_mapped_doc(
+        "Material Request",
+        material_request,
+        {
+            "Material Request": {
+                "doctype": "Purchase Approval"
+            },
+            "Material Request Item": {
+                "doctype": "Purchase Approval Item",
+                "condition": lambda source: source.item_code in selected_item_codes
+            }
+        }
+    )
+    
+    # Required field    
+    doc.supplier = supplier
+    doc.posting_date = now_datetime()
     doc.payment_method = payment_method
     doc.payment_note = payment_note
     doc.delivery_address = delivery_address
     doc.delivery_note = delivery_note
     doc.delivery_amount_note = delivery_amount_note
-
+    
+    for item in doc.items:
+        if item.item_code in qty_map:
+            item.qty = qty_map[item.item_code]
+    
     doc.insert()
     return doc.as_dict()
--- tahp/public/js/pms/material_request/material_request_button.js
+++ tahp/public/js/pms/material_request/material_request_button.js
@@ -3,13 +3,14 @@ frappe.ui.form.on("Material Request", {
         if (frm.doc.docstatus === 1) {
             frm.add_custom_button('Tạo trình duyệt mua hàng', function() {
                 show_purchase_request_dialog(frm);
-            }, __("Create"));
+            }).addClass('btn-primary');
         }
     }
 })
 
 function show_purchase_request_dialog(frm) {
     let dialog = new frappe.ui.Dialog({
+        size: 'large',
         title: 'Tạo trình duyệt mua hàng',
         fields: [
             {
@@ -18,51 +19,6 @@ function show_purchase_request_dialog(frm) {
                 fieldtype: 'Link',
                 options: 'Supplier',
                 reqd: 1
-            },
-            {
-                fieldname: 'material_request_type',
-                label: 'Lý do',
-                fieldtype: 'Select',
-                default: frm.doc.material_request_type,
-                read_only: 1,
-            },
-            {
-                fieldname: 'payment_method',
-                label: 'Phương thức thanh toán',
-                fieldtype: 'Select',
-                options: [
-                    "Chuyển khoản",
-                    "Tiền mặt"
-                ],
-                reqd: 1
-            },
-            {
-                fieldname: 'payment_note',
-                label: 'Thời hạn thanh toán',
-                fieldtype: 'Link',
-                options: 'Custom Payment Note',
-                reqd: 1
-            },
-            {
-                fieldname: 'delivery_address',
-                label: 'Địa chỉ giao hàng',
-                fieldtype: 'Link',
-                options: 'Address',
-                reqd: 1,
-            },
-            {
-                fieldname: 'delivery_note',
-                label: 'Thời gian giao hàng',
-                fieldtype: 'Link',
-                options: 'Custom Delivery Note',
-                reqd: 1,
-            },
-            {
-                fieldname: 'delivery_amount_note',
-                label: 'Chi phí vận chuyển',
-                fieldtype: 'Link',
-                options: 'Custom Delivery Cost',
-                reqd: 1,
             },
             {
                 fieldname: 'items',
@@ -107,19 +63,67 @@ function show_purchase_request_dialog(frm) {
                     qty: item.qty,
                     uom: item.uom,
                 }))
-            }
+            },
+            {
+                fieldname: 'recommend_reason',
+                label: 'Lý do chọn nhà cung cấp',
+                fieldtype: 'Small Text',
+                reqd: 1,
+            },
+            {
+                fieldname: 'payment_method',
+                label: 'Phương thức thanh toán',
+                fieldtype: 'Select',
+                options: [
+                    "Chuyển khoản",
+                    "Tiền mặt"
+                ],
+                reqd: 1
+            },
+            {
+                fieldname: 'payment_note',
+                label: 'Thời hạn thanh toán',
+                fieldtype: 'Link',
+                options: 'Custom Payment Note',
+                reqd: 1
+            },
+            {
+                fieldname: 'delivery_address',
+                label: 'Địa chỉ giao hàng',
+                fieldtype: 'Link',
+                options: 'Address',
+                reqd: 1,
+            },
+            {
+                fieldname: 'delivery_note',
+                label: 'Thời gian giao hàng',
+                fieldtype: 'Link',
+                options: 'Custom Delivery Note',
+                reqd: 1,
+            },
+            {
+                fieldname: 'delivery_amount_note',
+                label: 'Chi phí vận chuyển',
+                fieldtype: 'Link',
+                options: 'Custom Delivery Cost',
+                reqd: 1,
+            },
+
         ],
         primary_action_label: 'Xác nhận',
         primary_action: async function(values) {
-            console.log(values);
             try {
                 dialog.disable_primary_action();
+
+                const selected_items = values.items.map(item => ({
+                    item_code: item.item_code,
+                    qty: item.qty
+                }));
 
                 const purchase_approval = await create_purchase_approval(
                     frm.doc.name,
                     values.supplier,
-                    values.items,
-
+                    selected_items,
                     values.payment_method,
                     values.payment_note,
                     values.delivery_address,
@@ -146,14 +150,13 @@ function show_purchase_request_dialog(frm) {
     dialog.show()
 }
 
-async function create_purchase_approval(material_request, supplier, items, payment_method, payment_note, delivery_address, delivery_note, delivery_amount_note) {
+async function create_purchase_approval(material_request, supplier, selected_items, payment_method, payment_note, delivery_address, delivery_note, delivery_amount_note) {
     const response = await frappe.call({
         method: 'tahp.pms.doc_events.material_request.material_request_button.create_purchase_approval',
         args: {
             material_request: material_request,
             supplier: supplier,
-            items: items,
-
+            selected_items: selected_items,
             payment_method: payment_method,
             payment_note: payment_note,
             delivery_address: delivery_address,
